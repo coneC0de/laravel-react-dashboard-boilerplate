36878d4de84745d5755516b4fb6b0ffd3b684cd9
coneC0de <nemanjakg22@gmail.com>
2024-10-19 18:40:23 +0200

    migrations added model setup done controller added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,13 +13,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        \App\Models\User::create([
+        User::create([
+            'avatar' => 'images/avatar.png',
             'firstname' => 'Demo',
             'lastname' => 'Demo',
             'email' => 'demo@demo.com',
             'password' => \Illuminate\Support\Facades\Hash::make("123456"),
+            'role' => 'user'
         ]);
+
+        User::create([
+            'avatar' => 'images/avatar.png',
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => \Illuminate\Support\Facades\Hash::make("123456"),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'avatar' => 'images/avatar.png',
+            'firstname' => 'Super',
+            'lastname' => 'Admin',
+            'email' => 'super@admin.com',
+            'password' => \Illuminate\Support\Facades\Hash::make("123456"),
+            'role' => 'super_admin'
+        ]);
+
     }
 }
